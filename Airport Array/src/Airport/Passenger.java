5a859035e1f640f4e7c3b36746304990ca1b320c
Merab Tato Kutalia <tatocaster@users.noreply.github.com>
2017-10-30 13:28:56 +0400

    added meaningful comments

--- Airport Array/src/Airport/Passenger.java
+++ Airport Array/src/Airport/Passenger.java
@@ -1,32 +1,57 @@
 package Airport;
 
+
 public class Passenger {
-	private String firstName;
-	private String lastName;
-	private int time;
-	
-	public void setfirstName(String firstName){
-		this.firstName = firstName;
-	}
-	
-	public String getfirstName(){
-		return this.firstName;
-	}
-	
-	public void setlastName(String lastName){
-		this.lastName = lastName;
-	}
-	
-	public String getlastName(){
-		return this.lastName;
-	}
-	
-	public void setSecondInQueue(int time){
-		this.time = time;
-	}
-	
-	public int getSecondInQueue(){
-		return this.time;
-	}
+    private String firstName;
+    private String lastName;
+    private int time;
+
+    /**
+     * sets the firstname
+     *
+     * @param firstName
+     */
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the firstname of String type
+     */
+    public String getfirstName() {
+        return this.firstName;
+    }
+
+    /**
+     * sets the lastname
+     *
+     * @param lastName
+     */
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return lastname of String type
+     */
+    public String getlastName() {
+        return this.lastName;
+    }
+
+    /**
+     * sets the time
+     *
+     * @param time
+     */
+    public void setSecondInQueue(int time) {
+        this.time = time;
+    }
+
+    /**
+     * @return time of integer type
+     */
+    public int getSecondInQueue() {
+        return this.time;
+    }
 
 }
